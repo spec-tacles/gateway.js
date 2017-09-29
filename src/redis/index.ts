24d9b3f62cf3b304140a4b35fdc6431552073268
Truency <me@nomsy.net>
2017-09-29 02:37:52 -0400

    refactor #1

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -1,11 +1,12 @@
-import redis = require('redis');
+import { RedisClient, Multi } from 'redis';
+
 import tsubaki = require('tsubaki');
 import Client from '../core/Client';
 
-tsubaki.promisifyAll(redis.RedisClient.prototype);
-tsubaki.promisifyAll(redis.Multi.prototype);
+tsubaki.promisifyAll(RedisClient.prototype);
+tsubaki.promisifyAll(Multi.prototype);
 
-export default class Redis extends (<{ new(): any }> redis.RedisClient) {
+export default class Redis extends (<{ new(): any }> RedisClient) {
   public readonly c: Client;
 
   constructor(c: Client) {
@@ -35,38 +36,38 @@ export default class Redis extends (<{ new(): any }> redis.RedisClient) {
     });
   }
 
-  public async insertUser(user: any) {
+  public async insertUser(user: any): Promise<void> {
     await this.hmsetAsync(`user:${user.id}`, Redis.flatten(user));
   }
 
-  public async insertGame(userID: string, guildID: string, game: any) {
+  public async insertGame(userID: string, guildID: string, game: any): Promise<void> {
     await this.hmsetAsync(`user:${userID}:${guildID}:game`, Redis.flatten(game));
   }
 
-  public async insertStatus(userID: string, guildID: string, status: string) {
+  public async insertStatus(userID: string, guildID: string, status: string): Promise<void> {
     await this.setAsync(`user:${userID}:${guildID}:status`, status);
   }
 
-  public async insertGuild(guild: any) {
+  public async insertGuild(guild: any): Promise<void> {
     await this.hmsetAsync(`guild:${guild.id}`, Redis.flatten(guild));
     if (!guild.unavailable) await Promise.all(guild.roles.map((r: any) => this.insertRole(guild.id, r)));
   }
 
-  public async insertEmoji(guildID: string, emoji: any) {
+  public async insertEmoji(guildID: string, emoji: any): Promise<void> {
     await this.hmsetAsync(`guild:${guildID}:emoji:${emoji.id}`, Redis.flatten(emoji));
     await this.saddAsync(`guild:${guildID}:emoji:${emoji.id}:roles`, ...this.emoji.roles);
   }
 
-  public async insertRole(guildID: string, role: any) {
+  public async insertRole(guildID: string, role: any): Promise<void> {
     await this.hmsetAsync(`guild:${guildID}:role:${role.id}`, Redis.flatten(role));
   }
 
-  public async insertChannel(channel: any) {
+  public async insertChannel(channel: any): Promise<void> {
     if (channel.guild_id) await this.saddAsync(`guild:${channel.guild_id}:channel`, channel.id);
     await this.hmsetAsync(`channel:${channel.id}`, Redis.flatten(channel));
   }
 
-  public static flatten(obj: any) {
+  public static flatten(obj: any): void {
     const out: any = {};
     for (const k of Object.keys(obj)) {
       const type = typeof obj[k];
